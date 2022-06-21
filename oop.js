a282480cdc597770cef7206b8e3bd9056b0df0b1
Mike-casper <misha7709123@gmail.com>
2022-06-21 22:33:15 +0300

    Update oop.js

--- oop.js
+++ oop.js
@@ -6,9 +6,15 @@ let fiat ={
   passengers:3,
   mileage:80000,
   started: false,
+  fuel:0,
 
   start:function(){
     this.started = true;
+    if(this.fuel == 0){
+      alert("Your car is empty, fill up before starting!");
+    } else{
+      this.started = true;
+    }
   },
   stop:function(){
     this.started = false;
@@ -16,10 +22,20 @@ let fiat ={
 
   drive: function(){
     if (this.started){
+      if(this.fuel>0){
+        alert(this.make + " " + this.model + " goes zoom zoom");
+        this.fuel = this.fuel - 1;
+      }else{
+        alert("Oh no, out of fuel");
+        this.stop();
+      }
       alert("Zoom zoom!");
     } else {
       alert("You need to start the engine first.");
     }
+  },
+  addFuel:function(amount){
+    this.fuel = this.fuel + amount;
   }
 };
 
@@ -28,8 +44,54 @@ fiat.needsWashing = true;
 
 let cadi ={
   make: "GM",
-  
-}
+  model:"Cadillac",
+  year: 1955,
+  color:"tan",
+  passengers: 5,
+  convertible: false,
+  mileage:12989,
+  started: false,
+
+  start:function(){
+    this.started = true;
+  },
+  stop:function(){
+    this.started = false;
+  },
+  drive:function(){
+    if(this.started){
+      alert(this.make + " " + this.model + " goes zoom zoom!");
+    } else {
+      alert("You need to start the engine first.");
+    }
+  }
+};
+
+let chevy ={
+  make: "Chevy",
+  model:"Bel Air",
+  year: 1957,
+  color:"red",
+  passengers: 2,
+  convertible: false,
+  mileage:1021,
+  started: false,
+
+  start:function(){
+    this.started = true;
+  },
+  stop:function(){
+    this.started = false;
+  },
+  drive:function(){
+    if(this.started){
+      alert(this.make + " " + this.model + " goes zoom zoom!");
+    } else {
+      alert("You need to start the engine first.");
+    }
+  }
+};
+
 
 
 /*let miles = fiat.mileage;
