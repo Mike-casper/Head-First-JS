b63468afe4252d27801237f81234834d38b9b017
Mike-casper <misha7709123@gmail.com>
2022-06-23 22:52:44 +0300

    pgseven
    typeof

---
+++ training.js
@@ -0,0 +1,20 @@
+let test1 = "abdced";
+let test2 = "123";
+let test3 = true;
+let test4 = {};
+let test5 = [];
+let test6;
+let test7 = {"abdced":123};
+let test8 = ["abdced",123];
+let test9 = null;
+
+
+console.log(typeof test1);
+console.log(typeof test2);
+console.log(typeof test3);
+console.log(typeof test4);
+console.log(typeof test5);
+console.log(typeof test6);
+console.log(typeof test7);
+console.log(typeof test8);
+console.log(typeof test9);
